91677265a0e7da85211e12bb311ad1379e08fd20
Ain0el <92295911+Ain0el@users.noreply.github.com>
2026-04-09 10:11:28 +0700

    Fix Feature Register and Login

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -23,19 +23,20 @@ test('user can login', async ({ page }) => {
 test('login shows error when password is empty', async ({ page }) => {
   await login.textBoxEmail.waitFor({ state: 'visible' });
   await login.textBoxEmail.fill('fibey50141@nazisat.com');
+  await login.textBoxPassword.waitFor({ state: 'visible' });
   await login.textBoxPassword.fill('');
-  await login.textBoxPassword.press('Tab');
   await login.btnMasuk.click();
   await expect(login.textInputPass).toBeVisible();
 });
 
 test('login shows error when email is empty', async ({ page }) => {
+  await login.textBoxEmail.waitFor({ state: 'visible' });
   await login.textBoxEmail.fill('');
   await login.textBoxEmail.press('Tab');
   await login.textBoxPassword.waitFor({ state: 'visible' });
   await login.textBoxPassword.fill('Fibey50141');
   await login.btnMasuk.click();
-  await expect(login.textInputEmail).toBeVisible();
+  await expect(login.textInputEmail).toBeVisible({timeout: 2000});
 });
 
 test('User fails to login if email or password is incorrect', async ({ page }) => {
@@ -44,7 +45,6 @@ test('User fails to login if email or password is incorrect', async ({ page }) =
   await login.textBoxPassword.waitFor({ state: 'visible' });
   await login.textBoxPassword.fill('Fibey50140');
   await login.btnMasuk.click();
-  await page.waitForTimeout(2000);
   await expect(login.textEmailPassSalah).toBeVisible();
 });
 
--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -19,22 +19,29 @@ test('user can register', async ({ page }) => {
   await register.textBoxEmail.fill('fibey50141@nazisat.com');
   await register.textBoxPassword.fill('Fibey50141');
   await register.textBoxPassConfirm.fill('Fibey50141');
-  await register.clickCheckbox.click();
-  await register.btnAgree.click()
+  await register.clickCheckbox.check();
+  await register.btnAgree.click();
+  await register.btnBuatAkun.click({ state: 'visible' });
   await register.btnBuatAkun.click();
 });
 
 test('registration fails when password does not contain eight characters', async ({ page }) => {
 await register.textNamaLengkap.waitFor({ state: 'visible' });
 await register.textNamaLengkap.fill('minad');
+await register.textNamaLengkap.press('Tab');
 await register.textBoxUsername.waitFor({ state: 'visible' });
 await register.textBoxUsername.fill('minadd');
+await register.textNamaLengkap.press('Tab');
 await register.textBoxEmail.waitFor({ state: 'visible' });
 await register.textBoxEmail.fill('minad99056@nazisat.com');
+await register.textBoxEmail.press('Tab');
 await register.textBoxPassword.fill('Minaddd');
+await register.textBoxPassword.press('Tab');
 await register.textBoxPassConfirm.fill('Minaddd');
+await register.textBoxPassConfirm.press('Tab');
 await register.clickCheckbox.check();
 await register.btnAgree.click();
+await register.btnBuatAkun.click({ state: 'visible' });
 await register.btnBuatAkun.click();
 await expect(register.textMinKarakterPass).toBeVisible();
  });
@@ -42,29 +49,41 @@ await expect(register.textMinKarakterPass).toBeVisible();
 test('registration fails when password does not contain uppercase letters', async ({ page }) => {
   await register.textNamaLengkap.waitFor({ state: 'visible' });
   await register.textNamaLengkap.fill('minad');
+  await register.textNamaLengkap.press('Tab');
   await register.textBoxUsername.waitFor({ state: 'visible' });
   await register.textBoxUsername.fill('minadd');
+  await register.textBoxUsername.press('Tab');
   await register.textBoxEmail.waitFor({ state: 'visible' });
   await register.textBoxEmail.fill('minad99056@nazisat.com');
+  await register.textBoxEmail.press('Tab');
   await register.textBoxPassword.fill('minaaddd');
+  await register.textBoxPassword.press('Tab');
   await register.textBoxPassConfirm.fill('minaaddd');
+  await register.textBoxPassConfirm.press('Tab');
   await register.clickCheckbox.check();
-  await register.btnAgree.click()
+  await register.btnAgree.click();
+  await register.btnBuatAkun.click({ state: 'visible' });
   await register.btnBuatAkun.click();
-  await expect(register.textHurufBesarPass).toBeVisible();
+  await expect(register.textHurufBesarPass).toBeVisible({ timeout: 2000 });
 });
 
 test('registration fails when password does not contain numbers', async ({ page }) => {
   await register.textNamaLengkap.waitFor({ state: 'visible' });
   await register.textNamaLengkap.fill('minad');
+  await register.textNamaLengkap.press('Tab');
   await register.textBoxUsername.waitFor({ state: 'visible' });
   await register.textBoxUsername.fill('minadd');
+  await register.textBoxUsername.press('Tab');
   await register.textBoxEmail.waitFor({ state: 'visible' });
   await register.textBoxEmail.fill('minad99056@nazisat.com');
+  await register.textBoxEmail.press('Tab');
   await register.textBoxPassword.fill('Minaaddd');
+  await register.textBoxPassword.press('Tab');
   await register.textBoxPassConfirm.fill('Minaaddd');
+  await register.textBoxPassConfirm.press('Tab');
   await register.clickCheckbox.click();
-  await register.btnAgree.click()
+  await register.btnAgree.click();
+  await register.btnBuatAkun.click({ state: 'visible' });
   await register.btnBuatAkun.click();
   await expect(register.textAngkaPass).toBeVisible();
 });
